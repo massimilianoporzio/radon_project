4ddd4a9b03c82f2bdc40720add36175ff8f49c59
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-11-27 11:00:15 +0100

    Chore: Register Custom User model in Admin

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User  # Assicurati che il modello si chiami User
+
+# Registriamo il tuo User custom usando la logica standard di Django
+# (così vedi i campi password, permessi, gruppi, ecc. formattati bene)
+admin.site.register(User, UserAdmin)
